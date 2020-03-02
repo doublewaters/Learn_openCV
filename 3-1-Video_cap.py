495fa76cf0bb7946e418829159b04fb41c241379
doublewaters <1103395299@qq.com>
2020-03-02 21:23:38 +0800

    3.1and3.2

---
+++ 3-1-Video_cap.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+#相关函数：cv2.VideoCapture()，cv2.VideoWrite()
+
+#用摄像头捕获视频
+
+cap = cv2.VideoCapture(0) 
+
+while(True):
+    # Capture frame-by-frame 
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+    # Our operations on the frame come here
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture 
+cap.release()
+cv2.destroyAllWindows()
+    
